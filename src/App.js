39e97992de59975e901e65e0a7cf2a7fa98bfbcf
jc-sf <junchao.zhang@salesforce.com>
2020-03-23 22:24:23 -0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from '@salesforce/design-system-react';
+
+import Header from './components/Header';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header userName="My Username" />
+      <div className="slds-m-top_xx-large slds-p-top_xx-large">
+        <div className="slds-x-small-buttons_horizontal slds-align_absolute-center">
+          <Button label="Base" variant="base" />
+          <Button label="New Subscription" />
+          <Button iconCategory="utility" iconName="download" iconPosition="left" label="Neutral Icon" />
+          <Button label="Responsive" variant="brand" responsive />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+App.propTypes = {};
+
+export default App;

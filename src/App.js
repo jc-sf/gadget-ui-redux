9f74831230c8cdf412b0c50b2ffd3bb4b9749e85
jc-sf <junchao.zhang@salesforce.com>
2020-04-09 11:33:51 -0700

    feat: Demo UI state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { useEffect } from 'react';
-import { Spinner } from '@salesforce/design-system-react';
+import { Spinner, Button, Checkbox } from '@salesforce/design-system-react';
 import { connect } from 'react-redux';
 import Header from './components/Header';
 import './App.scss';
 import { fetchRequests } from './modules/requests/actions';
+import { toggleButtonStatus } from './modules/ui/actions';
 
-const App = ({ requests, showSpinner, fetchRequests }) => {
+const App = ({ requestList, showSpinner, fetchRequests, showButton, toggleButtonStatus }) => {
   useEffect(() => {
-    fetchRequests();
+    // fetchRequests();
   }, [fetchRequests]);
+
+  const handleToggleButton = (e) => {
+    toggleButtonStatus();
+  };
 
   return (
     <div className="App">
       <Header userName="My Username" />
       <div className="slds-m-top_xx-large slds-p-top_xx-large">
         <div className="slds-x-small-buttons_horizontal slds-align_absolute-center">
-          {showSpinner && <Spinner size="large" variant="base" assistiveText={{ label: 'Main Frame Loading...' }} />}
+          {showButton && <Button variant="brand">Button</Button>}
         </div>
+        <div className="slds-x-small-buttons_horizontal slds-align_absolute-center">
+          <Checkbox
+            labels={{
+              label: 'Show/Hide Button',
+            }}
+            onChange={handleToggleButton}
+          ></Checkbox>
+        </div>
+
+        {showSpinner && <Spinner size="large" variant="base" assistiveText={{ label: 'Main Frame Loading...' }} />}
       </div>
     </div>
   );
@@ -27,6 +42,7 @@ App.propTypes = {};
 const mapStateToProps = (state) => ({
   requestList: state.requests,
   showSpinner: state.ui.showSpinner,
+  showButton: state.ui.showButton,
 });
 
-export default connect(mapStateToProps, { fetchRequests })(App);
+export default connect(mapStateToProps, { fetchRequests, toggleButtonStatus })(App);
--- src/modules/ui/reducer.js
+++ src/modules/ui/reducer.js
@@ -1,13 +1,16 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { fetchRequestsError, fetchRequestsStart } from '../requests/actions';
+import { toggleButtonStatus } from './actions';
 
 const initialState = {
   showSpinner: false,
   showErrorToast: false,
+  showButton: true,
 };
 const reducer = createReducer(initialState, {
   [fetchRequestsError]: (state, action) => ({ ...state, showErrorToast: true, showSpinner: false }),
   [fetchRequestsStart]: (state, action) => ({ ...state, showSpinner: true, showErrorToast: false }),
+  [toggleButtonStatus]: (state, action) => ({ ...state, showButton: !state.showButton }),
 });
 
 export default reducer;
